bd43f22c5f6b0191e2b83d83cd1fe408df7d69fa
PinkSickle <kitty@pinksickle.moe>
2023-09-19 18:14:10 -0700

    are death threats against github tos?

--- src/listshit.ts
+++ src/listshit.ts
@@ -1,2 +1,9 @@
-import '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
+const supabase = createClient('https://peqateslmhoxphhdnjzt.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlcWF0ZXNsbWhveHBoaGRuanp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkwNDE3MjcsImV4cCI6MjAwNDYxNzcyN30.JMDi2JTqcjKNX2XY9cAkt-zD-rsKOgqn4-GNhf9kPjQ')
+
+let { data: Levels, error } = await supabase
+  .from('Levels')
+  .select('*')
+
+export default Levels
